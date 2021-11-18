f6d2f70559d58a8062baeb308215a4441f20f925
recepdogan88 <88249451+recepdogan88@users.noreply.github.com>
2021-11-18 20:36:02 +0000

    it has been done

---
+++ Week-2/index2.js
@@ -0,0 +1,18 @@
+let x = "Recep";
+console.log(typeof x);
+alert(x);
+
+let age = 20;
+console.log(typeof age);
+document.write(age);
+
+let y = true;
+console.log(typeof y);
+document.write(y);
+
+let z;
+console.log(typeof z);
+document.write(z);
+
+let a = ["ali", "ahmet"];
+console.log(typeof a);
