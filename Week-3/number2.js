01f55c7736e756dd298259f8fdc930806d0928fb
recepdogan88 <88249451+recepdogan88@users.noreply.github.com>
2021-11-25 12:43:21 +0000

    Algorithm has been added.

--- Week-3/number2.js
+++ Week-3/number2.js
@@ -1,13 +1,8 @@
 /*
 1-Take the two numbers that the user wants to compare.
 2-Assign these variables as numbers.
-3-As a first condition, make the first number less than the second number.
-4-If the condition is met, show it in the browser and console.
-5-As the second condition, the first number is greater than the second number.
-6-If the condition is met, show it in the browser and console.
-7-Write the case where both numbers are equal.
-8-When both numbers are equal, show that the two numbers are equal in the browser and console.
-9-If the user does not enter a number and presses enter, 'Please enter a number' is warned.
+3-Create conditions to sort from largest to smallest.
+4-Write the terms of equality.
 */
 
 //Two numbers that the user wanted to compare were received.
